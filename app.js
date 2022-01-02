297dc9bcd47213f4efb36e4fef3a8d710231db61
Emily Dokken <dokkenemily@gmail.com>
2022-01-02 11:42:37 -0800

    obtain user input for portfolio using node and command line

---
+++ app.js
@@ -0,0 +1,9 @@
+const proflieDataArgs = process.argv.slice(2, process.argv.length);
+
+const printProfileData = profileDataArr => {
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+
+};
+
+printProfileData(proflieDataArgs);
